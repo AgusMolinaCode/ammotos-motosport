6906e855cc9ff3b3107a81610d242cd98290e527
Agustin Molina <am.motos@hotmail.com>
2026-01-18 12:53:09 -0300

    ok

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
                 role="link"
                 className="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100 cursor-pointer"
               >
-                Home
+                Hom
               </button>
             </Link>
 
