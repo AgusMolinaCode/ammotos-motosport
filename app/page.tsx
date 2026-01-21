4fc711c7a0b9b41d58135a64a0aefb54053d566a
Agustin Molina <am.motos@hotmail.com>
2026-01-21 08:34:41 -0300

    Eliminar importaciones no utilizadas de SmallBanner, Navbar y SearchBar en page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import SmallBanner from "@/components/main/SmallBanner";
-import Navbar from "@/components/main/Navbar";
-import SearchBar from "@/components/main/SearchBar";
 import HeroTwo from "@/components/main/HeroTwo";
 import SliderBrands from "@/components/main/SliderBrands";
 import OfferItems from "@/components/main/OfferItems";
